f1653ce77327880995afa4eb96bdd284bd174363
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-08-27 15:00:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function debounce(callback, delay, immediate = false) {
-  
-  }
-  
-  module.exports = debounce;
+    let timeout;
+
+    return function(...args) {
+        const context = this;
+
+        const later = () => {
+            timeout = null;
+            if (!immediate) callback.apply(context, args);
+        };
+
+        const callNow = immediate && !timeout;
+
+        clearTimeout(timeout);
+        timeout = setTimeout(later, delay);
+
+        if (callNow) callback.apply(context, args);
+    };
+}
+
+module.exports = debounce;
